23025bfc8f15ff92a7455d0cacf3eb22a62e2286
Alexis <sonntag.alexis@gmail.com>
2019-07-09 07:36:57 -0700

    update books database

--- books.sql
+++ books.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Erstellungszeit: 09. Jul 2019 um 12:26
+-- Erstellungszeit: 09. Jul 2019 um 14:35
 -- Server-Version: 5.7.26
 -- PHP-Version: 7.2.18
 
@@ -36,21 +36,35 @@ CREATE TABLE IF NOT EXISTS `books` (
   `category` varchar(30) NOT NULL,
   `description` varchar(150) NOT NULL,
   `price` decimal(10,0) NOT NULL,
-  PRIMARY KEY (`book_id`)
+  `format` varchar(50) NOT NULL,
+  `cover` varchar(60) NOT NULL,
+  `author_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`book_id`),
+  KEY `author_id` (`author_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=68 DEFAULT CHARSET=latin1;
 
 --
 -- Daten für Tabelle `books`
 --
 
-INSERT INTO `books` (`book_id`, `title`, `release_date`, `category`, `description`, `price`) VALUES
-(61, 'Lord of the Rings', '1954', 'Fantasy', '2 small guys destroy stolen jewellery of a homeless person', '35'),
-(62, 'The Hobbit', '1937', 'Fantasy', 'Introvert is forced to leave the house', '18'),
-(63, 'La Part de l\'Autre', '2001', 'Art', 'Alternate History of little Adolf', '18'),
-(64, 'La Peste', '1947', 'Art', 'World goes down', '19'),
-(65, 'Shakespear : Collected Works', '1602', 'Art', 'Murder and tragedy in fancy english', '43'),
-(66, 'Des Cannibales', '1557', 'Art', 'Something in french i\'d say', '13'),
-(67, 'Faust', '1808', 'Art', 'Gambling addiction of god and devil', '24');
+INSERT INTO `books` (`book_id`, `title`, `release_date`, `category`, `description`, `price`, `format`, `cover`, `author_id`) VALUES
+(61, 'Lord of the Rings', '1954', 'Fantasy', '2 small guys destroy stolen jewellery of a homeless person', '35', 'Hardback', 'C:\\wamp64\\www\\BookShopProject\\coverpics\\lotr_book.png', 1),
+(62, 'The Hobbit', '1937', 'Fantasy', 'Introvert is forced to leave the house', '18', 'Softcover', 'C:\\wamp64\\www\\BookShopProject\\coverpics\\hobbit_book.jpg', 1),
+(63, 'La Part de l\'Autre', '2001', 'Art', 'Alternate History of little Adolf', '18', 'Hardcover', 'C:\\wamp64\\www\\BookShopProject\\coverpics\\lpdla_book.jpg', 2),
+(64, 'La Peste', '1947', 'Art', 'World goes down', '19', 'Softcover', 'C:\\wamp64\\www\\BookShopProject\\coverpics\\peste_book.jpg', 3),
+(65, 'Shakespear : Collected Works', '1602', 'Art', 'Murder and tragedy in fancy english', '43', 'Hardback', 'C:\\wamp64\\www\\BookShopProject\\coverpics\\shakespear_book.jpg', 4),
+(66, 'Des Cannibales', '1557', 'Art', 'Something in french i\'d say', '13', 'Softcover', 'C:\\wamp64\\www\\BookShopProject\\coverpics\\cannibales_book.jpg', 5),
+(67, 'Faust', '1808', 'Art', 'Gambling addiction of god and devil', '24', 'Softcover', 'C:\\wamp64\\www\\BookShopProject\\coverpics\\faust_book.jpg', 6);
+
+--
+-- Constraints der exportierten Tabellen
+--
+
+--
+-- Constraints der Tabelle `books`
+--
+ALTER TABLE `books`
+  ADD CONSTRAINT `books_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `author` (`author_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
