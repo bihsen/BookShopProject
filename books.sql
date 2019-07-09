4ee5d316e46deec361d58c4268b464122e9a0df7
Alexis <sonntag.alexis@gmail.com>
2019-07-09 05:56:27 -0700

    update book db file

--- books.sql
+++ books.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jul 09, 2019 at 08:57 AM
--- Server version: 5.7.26
--- PHP Version: 7.2.18
+-- Erstellungszeit: 09. Jul 2019 um 12:26
+-- Server-Version: 5.7.26
+-- PHP-Version: 7.2.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,25 +19,38 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `bookstore`
+-- Datenbank: `bookstore`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `books`
+-- Tabellenstruktur für Tabelle `books`
 --
 
 DROP TABLE IF EXISTS `books`;
 CREATE TABLE IF NOT EXISTS `books` (
   `book_id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(30) NOT NULL,
-  `release_date` year(4) NOT NULL,
+  `release_date` varchar(30) NOT NULL,
   `category` varchar(30) NOT NULL,
-  `description` text NOT NULL,
-  `format` varchar(20) NOT NULL,
+  `description` varchar(150) NOT NULL,
+  `price` decimal(10,0) NOT NULL,
   PRIMARY KEY (`book_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=68 DEFAULT CHARSET=latin1;
+
+--
+-- Daten für Tabelle `books`
+--
+
+INSERT INTO `books` (`book_id`, `title`, `release_date`, `category`, `description`, `price`) VALUES
+(61, 'Lord of the Rings', '1954', 'Fantasy', '2 small guys destroy stolen jewellery of a homeless person', '35'),
+(62, 'The Hobbit', '1937', 'Fantasy', 'Introvert is forced to leave the house', '18'),
+(63, 'La Part de l\'Autre', '2001', 'Art', 'Alternate History of little Adolf', '18'),
+(64, 'La Peste', '1947', 'Art', 'World goes down', '19'),
+(65, 'Shakespear : Collected Works', '1602', 'Art', 'Murder and tragedy in fancy english', '43'),
+(66, 'Des Cannibales', '1557', 'Art', 'Something in french i\'d say', '13'),
+(67, 'Faust', '1808', 'Art', 'Gambling addiction of god and devil', '24');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
